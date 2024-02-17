633aa1853c71ade51ca96becaf1f1f21ff3486be
Hussien El-Sawy <hussien.elsawy@outlook.com>
2024-02-17 19:11:41 +0200

    Adding Search function for (datasources, dashboards, alerts) Adding Backup function for (alerts) Adding Update function for (datasoruces, dashboards, alerts)

--- start.py
+++ start.py
@@ -4,11 +4,17 @@ import system
 def main_menu():
     print("\nWhat you want to do today ?\n")
     options = ["Backup all datasources", 
-               "Backup all dashboards", 
+               "Backup all dashboards",
+               "Backup all alert-rules",
+               "Cahnge a value in all datasources",
+               "Change a value in all dashboards",
+               "Change a value in all alert-rules", 
                "Import all datasources",
                "Import all dashboards",
-               "Change a value in all dashboards",
-               "Search a value in all dashboards"]
+               "Import all alert-rules",
+               "Search a value in all datasources",
+               "Search a value in all dashboards",
+               "Search a value in all dalert-rules",]
     for idx, element in enumerate(options):
         print("{}- {}".format(idx + 1, element))
 
@@ -71,20 +77,40 @@ def start_grafana_sync(url=""):
         if response.status_code == 200:
             print("\nGood we are CONNECTED!")
             choice = main_menu()
-            if choice == 1: # backup all datasources
-                system.create_backup("datasources", url)
-                return_main_menu(url)
-            elif choice == 2: # backup all dashboards
-                system.create_backup("dashboards", url)
-                return_main_menu(url)
-            elif choice == 3: # import all datasources
-                print("choice 3")
-            elif choice == 4: # import all dashboards
-                print("choice 4")
-            elif choice == 5: # change values of all dashboards
-                print("choice 5")
-            elif choice == 6: # search values of all dashboards
-                print("choice 6")
+            if choice in (1,2,3): # backup all
+                if choice == 1:
+                    type = "datasources"
+                elif choice == 2:
+                    type = "dashboards"
+                elif choice == 3:
+                    type = "alerts"
+                system.create_backup(type, url)
+            
+            elif choice in (4,5,6): # update all
+                if choice == 4:
+                    type = "datasources"
+                elif choice == 5:
+                    type = "dashboards"
+                elif choice == 6:
+                    type = "alerts"
+                old_value = input("\nWrite the value you want to change: ")
+                new_value = input("Write the new value you want to add: ")
+                system.update_all(type, old_value, new_value, url)
+            
+            elif choice in (7,8,9) : # import all 
+                print("import all")
+            
+            elif choice in (10,11,12): # search all
+                if choice == 10:
+                    type = "datasources"
+                elif choice == 11:
+                    type = "dashboards"
+                elif choice == 12:
+                    type = "alerts"
+                value = input("Write the value you want to find: ")
+                system.search_all(type, value, url)
+
+            return_main_menu(url)
         else:
             print("\nCheck your username and password and try again.")
             start_grafana_sync()
@@ -97,7 +123,9 @@ print("\nWelcome to Grafana Sync")
 print("-----------------------")
 
 can_start = True
-for folder in ("backup",'backup\dashboards','backup\datasources'):
+for folder in ("backup",'backup\dashboards','backup\datasources','backup\\alerts',
+               "updated",'updated\dashboards','updated\datasources','updated\\alerts',
+               "search_results",'search_results\dashboards','search_results\datasources','search_results\\alerts'):
     create_result = system.create_folder(folder)
     if create_result != "Created":
         print(folder,create_result)
--- system.py
+++ system.py
@@ -6,32 +6,10 @@ from datetime import datetime
 
 app_path = os.path.dirname(os.path.abspath(__file__))
 
-def create_backup(type, url, cookies=""):
-    now = datetime.now().strftime("%Y%m%d%H%M%S")
-    headers = {'Cookie': cookies}
-        
-    print("Please wait while we are creating json files for all your "+type+"...")
-    if type == "datasources":
-        api_all =  "/api/datasources"
-        api_uid =  "/api/datasources/uid/"
-        backup_folder = "backup\datasources\\"+str(now)+"\\"
-    elif type == "dashboards":
-        api_all =  "/api/search?query=%"
-        api_uid =  "/api/dashboards/uid/"
-        backup_folder = "backup\dashboards\\"+str(now)+"\\"
-        
-    get_all = requests.get(url+api_all, headers=headers)
-    if create_folder(backup_folder) == "Created":
-        for get_each in get_all.json():
-            if type == "datasources":
-                get_each =  requests.get(url+api_uid+get_each["uid"], headers=headers).json()
-            elif type == "dashboards":
-                get_each =  requests.get(url+api_uid+get_each["uid"], headers=headers).json()["dashboard"]
-            create_file(backup_folder, get_each["uid"]+"_v"+str(get_each["version"])+".json", json.dumps(get_each))
-        print("Completed, Check folder ["+backup_folder+"].")
-    else:
-        print("Coudlnt create the folder, please try again")
-
+def create_file(folder, file_name, content):
+    f = open(app_path+"\\"+folder+"\\"+file_name, "a")
+    f.write(content)
+    f.close()
 
 
 def create_folder(folder_name):
@@ -42,16 +20,122 @@ def create_folder(folder_name):
             return str(e)
     return "Created"
 
-def create_file(folder, file_name, content):
-    f = open(app_path+"\\"+folder+"\\"+file_name, "a")
-    f.write(content)
-    f.close()
+
+def create_backup(type, url, cookies=""):
+    """
+    Connects to your grafana, crawl the data and creates backup files
+
+    Params: 
+     - type: string that matches one of the following (datasources, dashboards, alerts)
+     - url: string with grafana url (https://www.mygrafana.com)
+     - cookies: optional striing that matches your session cookies, can be used instead of username and password
+    """
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    headers = {'Cookie': cookies}
+    print("Please wait while we are creating json files for all your "+type+"...")
+    if type == "datasources":
+        api_all =  "/api/datasources"
+    elif type == "dashboards":
+        api_all =  "/api/search?query=%"
+    elif type == "alerts":
+        api_all = api_uid = "/api/v1/provisioning/alert-rules/"
+    else:
+        print("Error: type must be [alerts, dashboards or datasources]")
+        exit()
+    api_uid = "/api/"+type+"/uid/"
+    backup_folder = "backup\\"+type+"\\"+str(now)+"\\"
+    get_all = requests.get(url+api_all, headers=headers)
+    if create_folder(backup_folder) == "Created":
+        for get_each in get_all.json():
+            if type in ("datasources", "alerts"):
+                get_each = requests.get(url+api_uid+get_each["uid"], headers=headers).json()
+            elif type == "dashboards":
+                get_each = requests.get(url+api_uid+get_each["uid"], headers=headers).json()["dashboard"]
+            
+            create_file(backup_folder, get_each["uid"]+".json", json.dumps(get_each))
+        print("Completed, Check folder ["+backup_folder+"].")
+    else:
+        print("Coudlnt create the folder, please try again")
 
 
-def update_all_dashboards(url, cookie=""):
+def update_all(type, old_value, new_value, url, cookies=""):
     """
-    TO DO: update all the dashboards then export them as valid json files
+    Connects to your grafana, crawl the data and update the values mentioned and save the files
+
+    Params: 
+     - type: string that matches one of the following (datasources, dashboards, alerts)
+     - old_value: the value you want to change
+     - new_value: the new value you want to add
+     - url: string with grafana url (https://www.mygrafana.com)
+     - cookies: optional striing that matches your session cookies, can be used instead of username and password
     """
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    headers = {'Cookie': cookies}
+    print("Please wait while we update the values mentioned and save updated json files for all your "+type+"...")
+    api_uid = "/api/"+type+"/uid/"
+    update_folder = "updated\\"+type+"\\"+str(now)+"\\"
+
+    if type == "datasources":
+        api_all =  "/api/datasources"
+    elif type == "dashboards":
+        api_all =  "/api/search?query=%"
+    elif type == "alerts":
+        api_all = api_uid = "/api/v1/provisioning/alert-rules/"
+    else:
+        print("Error: type must be [alerts, dashboards or datasources]")
+        exit()
+    get_all = requests.get(url+api_all, headers=headers)
+    if create_folder(update_folder) == "Created":
+        for get_each in get_all.json():
+            if type in ("datasources", "alerts"):
+                get_each = requests.get(url+api_uid+get_each["uid"], headers=headers).json()
+            elif type == "dashboards":
+                get_each = requests.get(url+api_uid+get_each["uid"], headers=headers).json()["dashboard"]
+            if old_value in json.dumps(get_each):
+                get_each = json.dumps(get_each).replace(old_value, new_value)
+                create_file(update_folder, json.loads(get_each)["uid"]+".json", get_each)
+        print("Completed, Check folder ["+update_folder+"].")
+    else:
+        print("Coudlnt create the folder, please try again")
+
+
+def search_all(type, value, url, cookies=""):
+    """
+    Connects to your grafana, crawl the data and search the values mentioned and save the files if exist
+
+    Params: 
+     - type: string that matches one of the following (datasources, dashboards, alerts)
+     - value: the value you want to find
+     - url: string with grafana url (https://www.mygrafana.com)
+     - cookies: optional striing that matches your session cookies, can be used instead of username and password
+    """
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    headers = {'Cookie': cookies}
+    print("Please wait while we search the value mentioned and save result json files for all your "+type+"...")
+    api_uid = "/api/"+type+"/uid/"
+    search_folder = "search_results\\"+type+"\\"+str(now)+"\\"
+    if type == "datasources":
+        api_all =  "/api/datasources"
+    elif type == "dashboards":
+        api_all =  "/api/search?query=%"
+    elif type == "alerts":
+        api_all = api_uid = "/api/v1/provisioning/alert-rules/"
+    else:
+        print("Error: type must be [alerts, dashboards or datasources]")
+        exit()
+    get_all = requests.get(url+api_all, headers=headers)
+    if create_folder(search_folder) == "Created":
+        for get_each in get_all.json():
+            if type in ("datasources", "alerts"):
+                get_each = requests.get(url+api_uid+get_each["uid"], headers=headers).json()
+            elif type == "dashboards":
+                get_each = requests.get(url+api_uid+get_each["uid"], headers=headers).json()["dashboard"]
+            if value in json.dumps(get_each):
+                create_file(search_folder, get_each["uid"]+".json", json.dumps(get_each))
+        print("Completed, Check folder ["+search_folder+"].")
+    else:
+        print("Coudlnt create the folder, please try again")
+
 
 def import_all_datasources(url, cookie=""):
     """
@@ -63,7 +147,8 @@ def import_all_dashboards(url, cookie=""):
     TO DO: import all the dashboards json files to the url mentioned
     """
 
-def search_all_dashboards(url, cookie=""):
+
+def import_all_alerts(url, cookie=""):
     """
-    TO DO: search all the dashboards from the api directly
+    TO DO: import all the alerts json files to the url mentioned
     """
